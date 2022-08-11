fb3276203058d275f0274677e9302887c7e74801
Artem <66876952+Temkin88@users.noreply.github.com>
2022-08-11 18:20:09 +0300

    Update main_tg_bot.py

--- main_tg_bot.py
+++ main_tg_bot.py
@@ -3,166 +3,158 @@ import telebot
 import re
 import json
 import time
+
+from aiogram.types import update
 from telebot import apihelper, types
 
 
-# proxies = {"https": "hqproxyusr.avp.ru:8080", "http": "hqproxyusr.avp.ru:8080"}
-# apihelper.proxy = proxies
+proxies = {"https": "hqproxyusr.avp.ru:8080", "http": "hqproxyusr.avp.ru:8080"}
+apihelper.proxy = proxies
 
 with open('config.json') as f:
     templates = json.load(f)
     tg_bot_token = templates["tg_bot_token"]
     coin_token = templates["coin_token"]
 
+with open("symbol_all.json", "r", encoding="utf-8") as fn:
+    temp = json.load(fn)
+
 bot = telebot.TeleBot(tg_bot_token)
 
-region = []
-# wikipedia.set_lang("ru")
+reg = []
+
+
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Наберите команду /help')
+    bot.send_message(m.chat.id, 'Наберите команду /help, /menu')
 
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    with open("symbol_usd.json", "r", encoding="utf-8") as fn:
-        templates = json.load(fn)
-
     if message.text == "/help":
-        bot.send_message(message.from_user.id, f"Введите пример: '/btc 10'\n {', '.join(templates.keys())} ")
+
+        bot.send_message(message.from_user.id, f"Введите пример: '/btc 10'\n {', '.join(temp)} ")
+
     elif message.text == "/menu":
         keyboard = [
             [
-                types.InlineKeyboardButton("RUB", callback_data='RUB'),
-                types.InlineKeyboardButton("USD", callback_data='USD'),
+                types.InlineKeyboardButton(text='USD', callback_data='USD'),
+                types.InlineKeyboardButton(text='EUR', callback_data='EUR'),
+                types.InlineKeyboardButton(text='RUB', callback_data='RUB'),
+                types.InlineKeyboardButton(text='JPY', callback_data='JPY'),
             ],
-            [types.InlineKeyboardButton("Option 3", callback_data='3')],
+            [types.InlineKeyboardButton(text='CNY', callback_data='CNY'),
+             types.InlineKeyboardButton(text='KRW', callback_data='KRW'),
+             types.InlineKeyboardButton(text='TRY', callback_data='TRY'),
+             types.InlineKeyboardButton(text='IRR', callback_data='IRR'),
+            ],
         ]
         reply_markup = types.InlineKeyboardMarkup(keyboard)
-        bot.send_message(message.chat.id,
-                         text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(
-                             message.from_user), reply_markup=reply_markup)
-
+        bot.send_message(message.from_user.id, text='Выбери курс валют', reply_markup=reply_markup)
     else:
         coin_list = re.findall(r'\d+', message.text)
         coin_int = int(coin_list[0]) if coin_list else 1
         symbol = "".join(re.findall(r'[a-zA-Z]', message.text))
-        reg = region[0] if region else "RUB"
-        bot.send_message(message.chat.id, get_course(coin_int, symbol, reg))
+        reg1 = reg if reg else "RUB"
+        regist = get_url(reg1, symbol, temp)
+        bot.send_message(message.chat.id, get_course(message, coin_int, symbol.upper(), regist))
 
 
+# Обработчик нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
+def callback_worker(call):
     if call.data == "USD":
-        region.append(call.data)
-        bot.send_message(call.message.chat.id, f'Выюрали валюту {call.data}')
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
+    elif call.data == "EUR":
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
     elif call.data == "RUB":
-        region.append(call.data)
-        bot.send_message(call.message.chat.id, f'Выюрали валюту {call.data}')
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
+    elif call.data == "JPY":
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
+    elif call.data == "CNY":
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
+    elif call.data == "KRW":
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
+    elif call.data == "TRY":
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
+    elif call.data == "IRR":
+        reg.append(call.data)
+        bot.send_message(call.from_user.id, text=f'Выбрана валюта {call.data}')
 
 
-def percent(new: int, old: int):
-    return round((new - old) / old * 100, 2)
+def get_url(reg, symbol, temp):
 
 
-def get_symbol(message: list, parameter):
-    symbol = []
-    value = {}
-
-    for i in range(len(message)):
-        symbol.append(message[i]["symbol"])
-        value[message[i]["symbol"]] = {
-            "price": int(message[i]['quote'][parameter]['price']),
-            "volume_24h": int(message[i]['quote'][parameter]['volume_24h']),
-            "volume_change_24h": int(message[i]['quote'][parameter]['volume_change_24h']),
-            "percent_change_1h": message[i]['quote'][parameter]['percent_change_1h'],
-            "percent_change_24h": message[i]['quote'][parameter]['percent_change_24h'],
-        }
-
-        with open("symbol.json", 'w') as f:
-            f.write(json.dumps(value))
-    # if parameter == "RUB":
-    #     with open("symbol.json", 'w') as f:
-    #         f.write(json.dumps(value))
-    # else:
-    #     with open("symbol.json", 'w') as f:
-    #         f.write(json.dumps(value))
-
-    return symbol
-
-
-def get_course(coin, symbol, region):
+    param_list = {}
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/category'
-    parameter = {
-        "id": "605e2ce9d41eae1066535f7c",
-        'start': '1',
-        'limit': '1000',
-        'convert': region
-    }
-
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': coin_token,
     }
-
-    try:
-        #  proxies=proxies
-        # response_rub = requests.get(url, headers=headers, params=parameters_rub)
-        # time.sleep(1)
-        response = requests.get(url, headers=headers, params=parameter)
+    for i in range(len(reg)):
+        parameters = {
+            "id": "605e2ce9d41eae1066535f7c",
+            'start': '1',
+            'limit': '1000',
+            'convert': reg[i]
+        }
         time.sleep(1)
-        # data_rub = json.loads(response_rub.text)
+        response = requests.get(url, headers=headers, params=parameters, proxies=proxies)
+        time.sleep(1)
         data = json.loads(response.text)
-        # get_symbol(data["data"]["coins"], parameter['convert'])
-        symbols = get_symbol(data["data"]["coins"], parameter['convert'])
+        param = data["data"]["coins"]
+        param_list[reg[i]] = data
+        for j in range(len(param)):
+            if param[j]["symbol"] not in temp:
+                temp.append(f'/{param[j]["symbol"]}')
+                with open("symbol_all.json", "w", encoding="utf-8") as f:
+                    f.write(json.dumps(temp))
+            if symbol.upper() in param[j]["symbol"]:
+                param_list[reg[i]][param[j]["symbol"]] = {
+                    "price": int(param[i]['quote'][reg[i]]['price']),
+                    "volume_24h": int(param[j]['quote'][reg[i]]['volume_24h']),
+                    "volume_change_24h": int(param[j]['quote'][reg[i]]['volume_change_24h']),
+                    "percent_change_1h": param[j]['quote'][reg[i]]['percent_change_1h'],
+                    "percent_change_24h": param[j]['quote'][reg[i]]['percent_change_24h'],
+                }
+    return param_list
 
-        # with open("symbol_rub.json", "r", encoding="utf-8") as f:
-        #     templates_rub = json.load(f)
-
-        with open("symbol.json", "r", encoding="utf-8") as fn:
-            templates = json.load(fn)
+@bot.message_handler(content_types=["text"])
+def get_course(message, coin, symbol, regist):
+    try:
         sp = {}
         source = "{:,d}"
-        # for key, value in templates_rub.items():
-            # if key == symbol.upper():
-            #     price = source.format(round(value['price'] * coin, 4))
-            #     # volume_24h = source.format(round(value['volume_24h'] * coin, 4))
-            #     # volume_change_24h = source.format(round(value['volume_change_24h'] * coin, 4))
-            #     # percent_change_1h = round(value['percent_change_1h'], 2)
-            #     # percent_change_24h = round(value['percent_change_24h'], 2)
-            #
-            #     sp["RUB"] = {
-            #         "name": f"{key} {parameter['convert']}: {price}",
-            #         # "volume_24h": f"Значение 24h: {volume_24h}",
-            #         # "volume_change_24h": f"Значение обновлено 24h: {volume_change_24h}",
-            #         # "percent_change_1h": f"Проценты 1h: {percent_change_1h}%",
-            #         # "percent_change_24h": f"Проценты 24h: {percent_change_24h}%",
-            #     }
-        for key, value in templates.items():
-            if key == symbol.upper():
-                price = source.format(round(value['price'] * coin, 4))
-                volume_24h = source.format(round(value['volume_24h'] * coin, 4))
-                volume_change_24h = source.format(round(value['volume_change_24h'] * coin, 4))
-                percent_change_1h = round(value['percent_change_1h'], 2)
-                percent_change_24h = round(value['percent_change_24h'], 2)
 
-                sp[region] = {
-                    "name": f"{key} {parameter['convert']}: {price}",
-                    "volume_24h": f"Объёма торгов за 24h: {volume_24h}",
-                    "volume_change_24h": f"Изменение объёма торгов за 24h: {volume_change_24h}",
-                    "percent_change_1h": f"Проценты за 1h: {percent_change_1h}%",
-                    "percent_change_24h": f"Проценты за 24h: {percent_change_24h}%",
-                }
-        return "\n".join(
-            (
-            sp[region]["name"],
-            sp[region]["volume_24h"],
-            sp[region]["volume_change_24h"],
-            sp[region]["percent_change_1h"],
-            sp[region]["percent_change_24h"],)
-        )
+        for key, value in regist.items():
+            val = value.get(symbol)
+            price = source.format(round(val['price'] * coin, 4))
+            volume_24h = source.format(round(val['volume_24h'] * coin, 4))
+            volume_change_24h = source.format(round(val['volume_change_24h'] * coin, 4))
+            percent_change_1h = round(val['percent_change_1h'], 2)
+            percent_change_24h = round(val['percent_change_24h'], 2)
 
+            sp[key] = {
+                "name": f"{list(value)[2]} {key}: {price}",
+                "volume_24h": f"Объёма торгов за 24h: {volume_24h}",
+                "volume_change_24h": f"Изменение объёма торгов за 24h: {volume_change_24h}",
+                "percent_change_1h": f"Проценты за 1h: {percent_change_1h}%",
+                "percent_change_24h": f"Проценты за 24h: {percent_change_24h}%",
+            }
+            bot.send_message(message.chat.id, '\n'.join([
+                sp[key]["name"],
+                sp[key]["volume_24h"],
+                sp[key]["volume_change_24h"],
+                sp[key]["percent_change_1h"],
+                sp[key]["percent_change_24h"],
+            ]))
     except:
         return ("\U00002620 Что то пошло не так \U00002620")
 
